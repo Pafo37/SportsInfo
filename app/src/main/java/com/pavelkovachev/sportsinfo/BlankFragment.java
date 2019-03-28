6ef55d0cdb84325df82eef73ac2f096a7a163454
Pavel Kovachev <pavel.kovachev@adastragrp.com>
2019-03-28 17:17:24 +0200

    Initial commit Testing navigation graph and connect screens

---
+++ app/src/main/java/com/pavelkovachev/sportsinfo/BlankFragment.java
@@ -0,0 +1,46 @@
+package com.pavelkovachev.sportsinfo;
+
+import android.os.Bundle;
+import android.support.annotation.NonNull;
+import android.support.annotation.Nullable;
+import android.support.v4.app.Fragment;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.Button;
+
+import androidx.navigation.NavController;
+import androidx.navigation.Navigation;
+
+public class BlankFragment extends Fragment {
+
+
+    @Override
+    public void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+    }
+
+    @Override
+    public void onViewCreated(@NonNull View view, @Nullable Bundle savedInstanceState) {
+        super.onViewCreated(view, savedInstanceState);
+
+    }
+
+    @Override
+    public View onCreateView(LayoutInflater inflater, ViewGroup container,
+                             Bundle savedInstanceState) {
+        final View view = inflater.inflate(R.layout.fragment_blank, container, false);
+        Button button;
+        button = view.findViewById(R.id.button);
+        button.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                NavController controller = Navigation.findNavController(view);
+                controller.navigate(R.id.action_nav_host_to_fragment2);
+            }
+        });
+        return view;
+
+    }
+
+}
---
+++ app/src/main/java/com/pavelkovachev/sportsinfo/MainActivity.java
@@ -0,0 +1,17 @@
+package com.pavelkovachev.sportsinfo;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+import androidx.navigation.fragment.NavHostFragment;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+
+    }
+}
