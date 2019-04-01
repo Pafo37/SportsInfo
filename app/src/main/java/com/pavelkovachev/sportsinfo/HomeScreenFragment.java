f42ead0ea9e3ec5292f0121cc2fc446169f7afc9
Pavel Kovachev <pavel.kovachev@adastragrp.com>
2019-04-01 16:40:09 +0300

    Format code

--- app/src/main/java/com/pavelkovachev/sportsinfo/HomeScreenFragment.java
+++ app/src/main/java/com/pavelkovachev/sportsinfo/HomeScreenFragment.java
@@ -9,6 +9,7 @@ import android.view.View;
 import android.view.ViewGroup;
 
 public class HomeScreenFragment extends Fragment {
+
     @Nullable
     @Override
     public View onCreateView(@NonNull LayoutInflater inflater, @Nullable ViewGroup container, @Nullable Bundle savedInstanceState) {
