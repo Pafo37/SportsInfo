ae27bedf405cf24a89c997bc60599f3d0f9fe89a
Pavel Kovachev <pavel.kovachev@adastragrp.com>
2019-04-01 16:34:30 +0300

    Rename some ids

--- app/src/main/java/com/pavelkovachev/sportsinfo/HomeScreenFragment.java
+++ app/src/main/java/com/pavelkovachev/sportsinfo/HomeScreenFragment.java
@@ -3,11 +3,12 @@ package com.pavelkovachev.sportsinfo;
 import android.os.Bundle;
 import android.support.annotation.NonNull;
 import android.support.annotation.Nullable;
+import android.support.v4.app.Fragment;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
 
-public class HomeScreenFragment extends android.support.v4.app.Fragment {
+public class HomeScreenFragment extends Fragment {
     @Nullable
     @Override
     public View onCreateView(@NonNull LayoutInflater inflater, @Nullable ViewGroup container, @Nullable Bundle savedInstanceState) {
