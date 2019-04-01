b6ce9ff6f678aba6a40a922899a8e01a492f942e
Pavel Kovachev <pavel.kovachev@adastragrp.com>
2019-04-01 16:32:15 +0300

    Add icons for homescreen Add gradients for homescreen Add dimens, strings Change theme Add butterknife

--- app/src/main/java/com/pavelkovachev/sportsinfo/MainActivity.java
+++ app/src/main/java/com/pavelkovachev/sportsinfo/MainActivity.java
@@ -2,12 +2,21 @@ package com.pavelkovachev.sportsinfo;
 
 import android.os.Bundle;
 import android.support.v7.app.AppCompatActivity;
+import android.support.v7.widget.Toolbar;
+
+import butterknife.BindView;
+import butterknife.ButterKnife;
 
 public class MainActivity extends AppCompatActivity {
+
+    @BindView(R.id.toolbar)
+    Toolbar toolbar;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        ButterKnife.bind(this,this);
+        setSupportActionBar(toolbar);
     }
 }
