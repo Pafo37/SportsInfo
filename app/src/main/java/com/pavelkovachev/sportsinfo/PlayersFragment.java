c114e927ee1bd8f57efe323ede58a3b09304bbfe
Pavel Kovachev <pavel.kovachev@adastragrp.com>
2019-04-03 17:49:04 +0300

    Add Players Layout

--- app/src/main/java/com/pavelkovachev/sportsinfo/PlayersFragment.java
+++ app/src/main/java/com/pavelkovachev/sportsinfo/PlayersFragment.java
@@ -8,11 +8,11 @@ import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
 
-public class HomeScreenFragment extends Fragment {
+public class PlayersFragment extends Fragment {
 
     @Nullable
     @Override
     public View onCreateView(@NonNull LayoutInflater inflater, @Nullable ViewGroup container, @Nullable Bundle savedInstanceState) {
-        return inflater.inflate(R.layout.fragment_homescreen, container, false);
+        return inflater.inflate(R.layout.item_players, container, false);
     }
 }
