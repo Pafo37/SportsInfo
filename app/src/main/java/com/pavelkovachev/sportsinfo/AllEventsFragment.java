1422f5dea53a2092fbe204757a5edfe3ca9269c2
Pavel Kovachev <pavel.kovachev@adastragrp.com>
2019-04-09 10:15:39 +0300

    Delete old homescreen Delete old icons for bottom navigation bar Add all events layout

--- app/src/main/java/com/pavelkovachev/sportsinfo/AllEventsFragment.java
+++ app/src/main/java/com/pavelkovachev/sportsinfo/AllEventsFragment.java
@@ -8,11 +8,11 @@ import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
 
-public class SportsFragment extends Fragment {
+public class AllEventsFragment extends Fragment {
 
     @Nullable
     @Override
     public View onCreateView(@NonNull LayoutInflater inflater, @Nullable ViewGroup container, @Nullable Bundle savedInstanceState) {
-        return inflater.inflate(R.layout.fragment_sports, container, false);
+        return inflater.inflate(R.layout.fragment_all_events, container, false);
     }
 }
