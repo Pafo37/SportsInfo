414dac0a61056eb99e8c48cabf70004b48046997
Pavel Kovachev <pavel.kovachev@adastragrp.com>
2019-04-24 14:56:09 +0300

    Connect bottom navigation bar with navigation graph

--- app/src/main/java/com/pavelkovachev/sportsinfo/ui/activity/main/MainActivity.java
+++ app/src/main/java/com/pavelkovachev/sportsinfo/ui/activity/main/MainActivity.java
@@ -4,10 +4,15 @@ import android.databinding.DataBindingUtil;
 import android.os.Bundle;
 import android.support.v7.app.AppCompatActivity;
 
+import androidx.navigation.ui.NavigationUI;
+
+import com.ittianyu.bottomnavigationviewex.BottomNavigationViewEx;
 import com.pavelkovachev.sportsinfo.R;
 import com.pavelkovachev.sportsinfo.databinding.ActivityMainBinding;
 
 import dagger.android.AndroidInjection;
+
+import static androidx.navigation.Navigation.findNavController;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -18,5 +23,7 @@ public class MainActivity extends AppCompatActivity {
         ActivityMainBinding activityMainBinding = DataBindingUtil
                 .setContentView(this, R.layout.activity_main);
         setSupportActionBar(activityMainBinding.toolbar);
+        BottomNavigationViewEx bottomNavigationViewEx = findViewById(R.id.bottom_navigation_bar);
+        NavigationUI.setupWithNavController(bottomNavigationViewEx, findNavController(findViewById(R.id.container)));
     }
 }
