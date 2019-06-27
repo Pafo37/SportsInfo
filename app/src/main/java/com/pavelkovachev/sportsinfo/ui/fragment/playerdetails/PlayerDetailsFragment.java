615cc97169cc733f881932eca7fd83bf5d1d09cf
Pavel Kovachev <pavel.kovachev@adastragrp.com>
2019-06-27 16:51:34 +0300

    Implement player details

--- app/src/main/java/com/pavelkovachev/sportsinfo/ui/fragment/playerdetails/PlayerDetailsFragment.java
+++ app/src/main/java/com/pavelkovachev/sportsinfo/ui/fragment/playerdetails/PlayerDetailsFragment.java
@@ -1,5 +1,11 @@
 package com.pavelkovachev.sportsinfo.ui.fragment.playerdetails;
 
+import android.os.Bundle;
+import android.support.annotation.NonNull;
+import android.support.annotation.Nullable;
+import android.view.View;
+
+import com.pavelkovachev.sportsinfo.Constants;
 import com.pavelkovachev.sportsinfo.R;
 import com.pavelkovachev.sportsinfo.databinding.FragmentPlayerDetailsBinding;
 import com.pavelkovachev.sportsinfo.ui.fragment.base.BaseFragment;
@@ -14,5 +20,13 @@ public class PlayerDetailsFragment extends BaseFragment<PlayerDetailViewModel, F
     @Override
     protected Class<PlayerDetailViewModel> getViewModel() {
         return PlayerDetailViewModel.class;
+    }
+
+    @Override
+    public void onViewCreated(@NonNull View view, @Nullable Bundle savedInstanceState) {
+        super.onViewCreated(view, savedInstanceState);
+        String playerId = getArguments().getString(Constants.BUNDLE_PLAYER_ID);
+        String teamId = getArguments().getString(Constants.BUNDLE_TEAM_ID);
+        viewModel.getPlayerDetails(playerId, teamId);
     }
 }
--- app/src/main/java/com/pavelkovachev/sportsinfo/ui/fragment/players/PlayersFragment.java
+++ app/src/main/java/com/pavelkovachev/sportsinfo/ui/fragment/players/PlayersFragment.java
@@ -5,6 +5,9 @@ import android.support.annotation.NonNull;
 import android.support.annotation.Nullable;
 import android.view.View;
 
+import androidx.navigation.NavController;
+import androidx.navigation.Navigation;
+
 import com.pavelkovachev.sportsinfo.Constants;
 import com.pavelkovachev.sportsinfo.R;
 import com.pavelkovachev.sportsinfo.databinding.FragmentPlayerDetailsBinding;
@@ -26,6 +29,8 @@ public class PlayersFragment extends BaseFragment<PlayersViewModel, FragmentPlay
     public void onViewCreated(@NonNull View view, @Nullable Bundle savedInstanceState) {
         super.onViewCreated(view, savedInstanceState);
         String teamId = getArguments().getString(Constants.BUNDLE_TEAM_ID);
+        NavController navController = Navigation.findNavController(view);
+
         viewModel.getPlayers(teamId);
 
         viewModel.getIsErrorShown().observe(this, onError -> {
@@ -33,6 +38,16 @@ public class PlayersFragment extends BaseFragment<PlayersViewModel, FragmentPlay
                 showErrorDialog(getString(R.string.error_message_title),
                         getString(R.string.error_message_description));
                 viewModel.getIsErrorShown().setValue(false);
+            }
+        });
+
+        viewModel.getIsPlayerClicked().observe(this, isClicked -> {
+            if (isClicked) {
+                Bundle bundle = new Bundle();
+                bundle.putString(Constants.BUNDLE_PLAYER_ID, viewModel.getPlayerId().getValue());
+                bundle.putString(Constants.BUNDLE_TEAM_ID, teamId);
+                viewModel.getIsPlayerClicked().setValue(false);
+                navController.navigate(R.id.action_playersFragment_to_playerDetailsFragment, bundle);
             }
         });
     }
